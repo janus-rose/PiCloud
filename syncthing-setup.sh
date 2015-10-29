e8aba4964e7bd6c8cb99f24515006174fbd3e9ee
jsk <joshk@riseup.net>
2015-10-29 10:17:21 -0400

    Added syncthing startup script

--- syncthing-setup.sh
+++ syncthing-setup.sh
@@ -6,9 +6,23 @@ echo deb http://apt.syncthing.net/ syncthing release | sudo tee /etc/apt/sources
 apt-get update
 apt-get install syncthing xmlstarlet
 syncthing -generate=\$HOME/.config/syncthing/
-## Change config file for remote access + TLS
+## Change config file for remote GUI access + TLS
 xmlstarlet ed -L -u "/configuration/gui/@tls" -v true $HOME/.config/syncthing/config.xml
 xmlstarlet ed -L -u "/configuration/gui/address" -v 0.0.0.0:8384 $HOME/.config/syncthing/config.xml
-## To do: add something here to auto generate a startup script
-syncthing
-
+## Add Syncthing startup script
+wget -O /etc/init.d/syncthing https://raw.githubusercontent.com/lawfulintercept/PiCloud/master/syncthing-daemon
+chmod +x /etc/init.d/syncthing
+sudo update-rc.d syncthing defaults
+## Start Syncthing service
+service syncthing start
+echo ""
+echo "***Syncthing is now installed and running on your Pi***"
+echo ""
+echo "Access the interface by typing [Your Pi's IP address]:8384"
+echo "into your web browser's address bar."
+echo ""
+echo "If you want to start/stop/restart Syncthing, you can type:"
+echo ""
+echo "sudo service syncthing stop"
+echo "sudo service syncthing start"
+echo "sudo service syncthing restart"
