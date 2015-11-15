b47383c77040b133be9f03f3a22cfdae9ae97920
jsk <joshk@riseup.net>
2015-11-15 00:25:41 -0500

    make syncthing generate keys/config in correct directory

--- syncthing-setup.sh
+++ syncthing-setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ## Add key and repo
-	curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo deb http://apt.syncthing.net/ syncthing release | sudo tee /etc/apt/sources.list.d/syncthing-release.list
 ## Install Syncthing
-apt-get update
-apt-get install syncthing xmlstarlet
-syncthing -generate=~/.config/syncthing/
+sudo apt-get update
+sudo apt-get install syncthing xmlstarlet
+syncthing -generate="$HOME/.config/syncthing/"
 ## Add Syncthing startup script
 sudo wget -O /etc/init.d/syncthing https://raw.githubusercontent.com/lawfulintercept/PiCloud/master/syncthing-daemon
 sudo chmod +x /etc/init.d/syncthing
 sudo update-rc.d syncthing defaults
-## Change config file for remote GUI access + TLS
+## Configure for remote GUI access + TLS
 xmlstarlet ed -L -u "/configuration/gui/@tls" -v true ~/.config/syncthing/config.xml
 xmlstarlet ed -L -u "/configuration/gui/address" -v 0.0.0.0:8384 ~/.config/syncthing/config.xml
 ## Start Syncthing service
@@ -18,8 +18,9 @@ sudo service syncthing start
 echo ""
 echo "***Syncthing is now installed and running on your Pi***"
 echo ""
-echo "Access the interface by typing [Your Pi's IP address]:8384"
-echo "into your web browser's address bar."
+echo "Access the interface by typing "
+echo "https://Your.Pi's.IP.Address:8384 into your web browser's"
+echo "address bar (ignoring the security warnings)."
 echo ""
 echo "If you want to start/stop/restart Syncthing, you can type:"
 echo ""
